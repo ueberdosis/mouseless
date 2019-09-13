c69aad8f0cde5166f3c6c634eb0151cf39f0d1d8
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-09-13 23:05:50 +0200

    add vue cli boilerplate

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Courses from './views/Courses.vue'
+import Sketch from './views/Sketch.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'courses',
+      component: Courses,
+      children: [
+        {
+          path: 'profile',
+          name: 'courses.sketch',
+          component: Sketch,
+        }
+      ],
+    },
+  ]
+})
