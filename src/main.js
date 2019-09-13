4696fc036a538f869c6132eef19dd92d7b6cf1d4
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-09-13 23:15:56 +0200

    add electron-store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Store from 'electron-store'
 
 Vue.config.productionTip = false
+
+const store = new Store()
+
+// store.set('unicorn', '🦄');
+// console.log(store.get('unicorn'))
 
 new Vue({
   router,
