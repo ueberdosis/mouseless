3b34a728fcb8339bcd8606cf8feec48ba482075a
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-09-24 14:49:25 +0200

    improve key layout

--- src/data/apps/sketch.js
+++ src/data/apps/sketch.js
@@ -4,13 +4,53 @@ export default {
   color: '#FDD231',
   shortcuts: [
     {
-      title: 'New Artboard',
+      title: 'Artboard',
       shortcut: ['a'],
       level: 1,
     },
     {
-      title: 'New Rectangle',
-      shortcut: ['r'],
+      title: 'Oval',
+      shortcut: ['o'],
+      level: 1,
+    },
+    {
+      title: 'Line',
+      shortcut: ['l'],
+      level: 1,
+    },
+    {
+      title: 'Rounded Rectangle',
+      shortcut: ['u'],
+      level: 1,
+    },
+    {
+      title: 'Vector',
+      shortcut: ['v'],
+      level: 1,
+    },
+    {
+      title: 'Pencil',
+      shortcut: ['p'],
+      level: 1,
+    },
+    {
+      title: 'Text',
+      shortcut: ['t'],
+      level: 1,
+    },
+    {
+      title: 'Artboard',
+      shortcut: ['a'],
+      level: 1,
+    },
+    {
+      title: 'Slice',
+      shortcut: ['s'],
+      level: 1,
+    },
+    {
+      title: 'Hotspot',
+      shortcut: ['h'],
       level: 1,
     },
     {
