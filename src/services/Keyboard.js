d5fe3af06629882503ac284f5068632c58e0137b
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-09-25 16:35:27 +0200

    add enter support

--- src/services/Keyboard.js
+++ src/services/Keyboard.js
@@ -21,6 +21,7 @@ export default class Keyboard {
     right: 'ArrowRight',
     down: 'ArrowDown',
     left: 'ArrowLeft',
+    enter: 'Enter',
   }
 
   static formats = {
@@ -32,6 +33,7 @@ export default class Keyboard {
     ArrowRight: '→',
     ArrowDown: '↓',
     ArrowLeft: '←',
+    Enter: '↩',
   }
 
   static formatKeyCode(name) {
