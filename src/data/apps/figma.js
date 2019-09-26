b63a993af241b419fe217b8694e39a7543b3cf2e
Hans Pagel <hans.pagel@ueber.io>
2019-09-26 14:24:30 +0200

    add figma shortcuts

--- src/data/apps/figma.js
+++ src/data/apps/figma.js
@@ -2,5 +2,239 @@
 export default {
   id: 'figma',
   title: 'Figma',
-  shortcuts: [],
+  groups: [
+    {
+      title: 'Essential',
+      id: 'essential',
+      version: 1,
+      shortcuts: [
+        {
+          title: 'Show/Hide UI',
+          keys: ['meta', '\\'],
+        },
+        {
+          title: 'Pick Color',
+          keys: ['control', 'c'],
+        },
+        {
+          title: 'Search Menu',
+          keys: ['meta', '/'],
+        },
+      ],
+    },
+    {
+      title: 'Tools',
+      id: 'tools',
+      version: 1,
+      shortcuts: [
+        {
+          title: 'Move Tool',
+          keys: ['v'],
+        },
+        {
+          title: 'Frame Tool',
+          keys: ['f'],
+        },
+        {
+          title: 'Pen Tool',
+          keys: ['p'],
+        },
+        {
+          title: 'Pencil Tool',
+          keys: ['shift', 'p'],
+        },
+        {
+          title: 'Text Tool',
+          keys: ['t'],
+        },
+        {
+          title: 'Rectangle Tool',
+          keys: ['r'],
+        },
+        {
+          title: 'Ellipse Tool',
+          keys: ['o'],
+        },
+        {
+          title: 'Line Tool',
+          keys: ['l'],
+        },
+        {
+          title: 'Arrow Tool',
+          keys: ['shift', 'l'],
+        },
+        {
+          title: 'Add/Show Comments',
+          keys: ['c'],
+        },
+        {
+          title: 'Slice Tool',
+          keys: ['s'],
+        },
+      ],
+    },
+    {
+      title: 'View',
+      id: 'view',
+      version: 1,
+      shortcuts: [
+        {
+          title: 'Rulers',
+          keys: ['shift', 'r'],
+        },
+        {
+          title: 'Show Outlines',
+          keys: ['meta', 'y'],
+        },
+        {
+          title: 'Pixel Preview',
+          keys: ['control', 'p'],
+        },
+        {
+          title: 'Layout Grids',
+          keys: ['control', 'g'],
+        },
+        {
+          title: 'Pixel Grid',
+          keys: ['meta', '\''],
+        },
+        {
+          title: 'Show Multiplayer Cursors',
+          keys: ['option', 'meta', '\\'],
+        },
+        {
+          title: 'Show Layers',
+          keys: ['option', '1'],
+        },
+        {
+          title: 'Show Components',
+          keys: ['option', '2'],
+        },
+        {
+          title: 'Show Team Library',
+          keys: ['option', '3'],
+        },
+      ],
+    },
+    {
+      title: 'Zoom',
+      id: 'zoom',
+      version: 1,
+      shortcuts: [
+        // TODO: Unclear. Works only with mousedrag.
+        // {
+        //   title: 'Pan',
+        //   keys: ['space'],
+        // },
+        {
+          title: 'Zoom In',
+          keys: ['+'],
+        },
+        {
+          title: 'Zoom Out',
+          keys: ['-'],
+        },
+        {
+          title: 'Zoom to 100%',
+          keys: ['0'],
+        },
+        {
+          title: 'Zoom to Fit',
+          keys: ['1'],
+        },
+        {
+          title: 'Zoom to Selection',
+          keys: ['2'],
+        },
+        {
+          title: 'Zoom to Previous Frame',
+          keys: ['shift', 'n'],
+        },
+        {
+          title: 'Zoom to Next Frame',
+          keys: ['n'],
+        },
+        {
+          title: 'Previous Page',
+          keys: ['pageup'],
+        },
+        {
+          title: 'Next Page',
+          keys: ['pagedown'],
+        },
+        {
+          title: 'Find Previous Frame',
+          keys: ['fn', 'left'],
+        },
+        {
+          title: 'Find Next Frame',
+          keys: ['fn', 'right'],
+        },
+      ],
+    },
+    {
+      title: 'Text',
+      id: 'text',
+      version: 1,
+      shortcuts: [
+
+      ],
+    },
+    {
+      title: 'Shape',
+      id: 'shape',
+      version: 1,
+      shortcuts: [
+
+      ],
+    },
+    {
+      title: 'Selection',
+      id: 'selection',
+      version: 1,
+      shortcuts: [
+
+      ],
+    },
+    {
+      title: 'Cursor',
+      id: 'cursor',
+      version: 1,
+      shortcuts: [
+
+      ],
+    },
+    {
+      title: 'Edit',
+      id: 'edit',
+      version: 1,
+      shortcuts: [
+
+      ],
+    },
+    {
+      title: 'Transform',
+      id: 'transform',
+      version: 1,
+      shortcuts: [
+
+      ],
+    },
+    {
+      title: 'Arrange',
+      id: 'arrange',
+      version: 1,
+      shortcuts: [
+
+      ],
+    },
+    {
+      title: 'Components',
+      id: 'components',
+      version: 1,
+      shortcuts: [
+
+      ],
+    },
+  ],
 }
