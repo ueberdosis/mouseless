fba28136f8c6c138a4f32a80cdf458e167491940
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-09-27 22:19:57 +0200

    show impossible shortcuts

--- src/services/HealthCheck.js
+++ src/services/HealthCheck.js
@@ -5,6 +5,7 @@ export default new class {
 
   run() {
     this.findDuplicatedShortcuts()
+    this.findImpossibleShortcuts()
   }
 
   findDuplicatedShortcuts() {
@@ -19,11 +20,39 @@ export default new class {
           .map(id => shortcuts.filter(shortcut => shortcut.id === id))
 
         if (duplicatedShortcuts.length) {
-          console.error(`Duplicated shortcuts in ${app.title} → ${group.title}`)
+          console.warn(`Duplicated shortcuts in ${app.title} → ${group.title}`)
           console.table(duplicatedShortcuts.flat())
         }
       })
     })
+  }
+
+  findImpossibleShortcuts() {
+    const impossibleShortcuts = []
+
+    DB.apps.forEach(app => {
+      app.groups.forEach(group => {
+        const shortcuts = app.shortcutsByGroup(group.id)
+
+        shortcuts.forEach(shortcut => {
+          const duplicatedKeys = this.findDuplicatesInArray(shortcut.resolvedKeys)
+
+          if (duplicatedKeys.length) {
+            impossibleShortcuts.push({
+              app: app.title,
+              group: group.title,
+              title: shortcut.title,
+              keys: shortcut.resolvedKeys.join(', '),
+            })
+          }
+        })
+      })
+    })
+
+    if (impossibleShortcuts.length) {
+      console.warn(`${impossibleShortcuts.length} impossible shortcuts found`)
+      console.table(impossibleShortcuts)
+    }
   }
 
   findDuplicatesInArray(data = []) {
