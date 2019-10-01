4e35076b3226624b4f909aeace491e6c4463b3a8
Hans Pagel <hans.pagel@ueber.io>
2019-10-01 11:55:23 +0200

    add trello shortcuts

---
+++ src/data/apps/trello.js
@@ -0,0 +1,111 @@
+// https://trello.com/shortcuts
+//, Move Card to Adjacent List  ,.<>
+export default {
+  id: 'trello',
+  title: 'Trello',
+  groups: [
+    {
+      title: 'Common Actions',
+      id: 'common',
+      version: 1,
+      shortcuts: [
+        {
+          title: 'Open Header Boards Menu',
+          keys: ['b'],
+        },
+        {
+          title: 'Focus Search Box',
+          keys: ['/'],
+        },
+        {
+          title: 'Archive Card',
+          keys: ['c'],
+        },
+        {
+          title: 'Due Date',
+          keys: ['d'],
+        },
+        {
+          title: 'Quick Edit Mode',
+          keys: ['e'],
+        },
+        {
+          title: 'Close Menu / Cancel Editing',
+          keys: ['Escape'],
+        },
+        {
+          title: 'Save Text',
+          keys: ['Meta', 'Enter'],
+        },
+        {
+          title: 'Open Card',
+          keys: ['Enter'],
+        },
+        {
+          title: 'Open Card Filter Menu',
+          keys: ['f'],
+        },
+        {
+          title: 'Label',
+          keys: ['l'],
+        },
+        {
+          title: 'Toggle Label Names',
+          keys: [';'],
+        },
+        {
+          title: 'Add / Remove Members',
+          keys: ['m'],
+        },
+        {
+          title: 'Insert New Card',
+          keys: ['n'],
+        },
+        {
+          title: 'My Cards Filter',
+          keys: ['q'],
+        },
+        {
+          title: 'Watch',
+          keys: ['s'],
+        },
+        {
+          title: 'Assign Self',
+          keys: ['Space'],
+        },
+        {
+          title: 'Edit Title',
+          keys: ['t'],
+        },
+        {
+          title: 'Vote',
+          keys: ['v'],
+        },
+        {
+          title: 'Toggle Board Menu',
+          keys: ['w'],
+        },
+        {
+          title: 'Clear All Filters',
+          keys: ['x'],
+        },
+        {
+          title: 'Open Shortcuts Page',
+          keys: ['?'],
+        },
+        {
+          title: 'Copy Card',
+          keys: ['Meta', 'c'],
+        },
+        {
+          title: 'Move Card',
+          keys: ['Meta', 'x'],
+        },
+        {
+          title: 'Paste Card',
+          keys: ['Meta', 'v'],
+        },
+      ],
+    },
+  ],
+}
