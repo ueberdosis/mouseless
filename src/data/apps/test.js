773c1f055486a02f6c8d3f391413f073fdcd0dc9
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-09 10:30:13 +0200

    update test shortcut

--- src/data/apps/test.js
+++ src/data/apps/test.js
@@ -23,8 +23,8 @@ export default {
         // {
         //   title: 'Multiple shortcuts',
         //   keys: [
-        //     ['='],
-        //     ['+'],
+        //     ['Meta', '='],
+        //     ['Meta', '+'],
         //   ],
         // },
       ],
