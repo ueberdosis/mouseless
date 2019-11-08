faf2dc778f04490a5892a851494996ae8bc6395d
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-11-08 14:39:27 +0100

    refactoring

--- src/notarize.js
+++ src/notarize.js
@@ -1,19 +1,22 @@
 require('dotenv').config()
 const { notarize } = require('electron-notarize')
 
-exports.default = async function (context) {
+module.exports = async context => {
   const { electronPlatformName, appOutDir } = context
+  const appName = context.packager.appInfo.productFilename
 
   if (electronPlatformName !== 'darwin') {
     return
   }
 
-  const appName = context.packager.appInfo.productFilename
-
-  await notarize({
-    appBundleId: 'com.Mouseless.app',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
-  })
+  try {
+    await notarize({
+      appBundleId: 'com.Mouseless.app',
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASS,
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
