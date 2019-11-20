2bed00a5363a7adfd1f237c4735f63934ae93b7f
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-11-20 12:12:12 +0100

    hass

--- src/services/Store.js
+++ src/services/Store.js
@@ -2,6 +2,10 @@ import Store from 'electron-store'
 import { app, remote } from 'electron'
 
 export default new Store({
+  // configName: process.env.NODE_ENV === 'development'
+  //   ? 'config_dev'
+  //   : 'config',
+  // encryptionKey: '123',
   projectVersion: (app && app.getVersion)
     ? app.getVersion()
     : remote.app.getVersion(),
