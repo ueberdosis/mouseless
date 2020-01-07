6d180851a3036e9d472ba4e8c25e3653406fde66
Hans Pagel <hans.pagel@ueber.io>
2020-01-07 15:27:24 +0100

    add spotify shortcuts

---
+++ src/apps/spotify.js
@@ -0,0 +1,137 @@
+/* eslint-disable max-len */
+// https://support.spotify.com/us/using_spotify/system_settings/keyboard-shortcuts/
+module.exports = {
+  id: 'spotify',
+  title: 'Spotify',
+  category: 'Entertainment',
+  description: null,
+  sets: [
+    {
+      title: 'Essentials',
+      id: 'essentials',
+      version: 1,
+      shortcuts: [
+        {
+          title: 'Create new playlist',
+          keys: ['Meta', 'n'],
+        },
+        {
+          title: 'Cut',
+          keys: ['Meta', 'x'],
+        },
+        {
+          title: 'Copy',
+          keys: ['Meta', 'c'],
+        },
+        {
+          title: 'Copy (alternative link)',
+          keys: ['Meta', 'Alt', 'c'],
+        },
+        {
+          title: 'Paste',
+          keys: ['Meta', 'v'],
+        },
+        {
+          title: 'Delete',
+          keys: ['Backspace'],
+        },
+        {
+          title: 'Select all',
+          keys: ['Meta', 'a'],
+        },
+        {
+          title: 'Play/pause',
+          keys: ['Space'],
+        },
+        {
+          title: 'Repeat',
+          keys: ['Meta', 'r'],
+        },
+        {
+          title: 'Shuffle',
+          keys: ['Meta', 's'],
+        },
+        {
+          title: 'Next track',
+          keys: ['Ctrl', 'Meta', 'ArrowRight'],
+        },
+        {
+          title: 'Previous track',
+          keys: ['Ctrl', 'Meta', 'ArrowLeft'],
+        },
+        {
+          title: 'Volume up',
+          keys: ['Meta', 'ArrowUp'],
+        },
+        {
+          title: 'Volume down',
+          keys: ['Meta', 'ArrowDown'],
+        },
+        {
+          title: 'Mute',
+          keys: ['Meta', 'Shift', 'ArrowDown'],
+        },
+        {
+          title: 'Max volume',
+          keys: ['Meta', 'Shift', 'ArrowUp'],
+        },
+        {
+          title: 'Show help',
+          keys: ['Meta', 'Shift', '?'],
+        },
+        {
+          title: 'Filter (in Songs and Playlists)',
+          keys: ['Meta', 'f'],
+        },
+        {
+          title: 'Give focus to search field',
+          keys: ['Meta', 'Alt', 'f'],
+        },
+        {
+          title: 'Go back',
+          keys: ['Meta', 'Alt', 'ArrowLeft'],
+        },
+        {
+          title: 'Go forward',
+          keys: ['Meta', 'Alt', 'ArrowRight'],
+        },
+        {
+          title: 'Play selected row',
+          keys: ['Enter'],
+        },
+        {
+          title: 'Preferences',
+          keys: ['Meta', ','],
+        },
+        {
+          title: 'Logout active user',
+          keys: ['Meta', 'Shift', 'w'],
+        },
+        {
+          title: 'Quit',
+          keys: ['Meta', 'q'],
+        },
+        {
+          title: 'Hide window',
+          keys: ['Meta', 'h'],
+        },
+        {
+          title: 'Hide other applications’ windows',
+          keys: ['Meta', 'Alt', 'h'],
+        },
+        {
+          title: 'Close window',
+          keys: ['Meta', 'w'],
+        },
+        {
+          title: 'Minimize window',
+          keys: ['Meta', 'm'],
+        },
+        {
+          title: 'Restore from minimized',
+          keys: ['Meta', 'Alt', '1'],
+        },
+      ],
+    },
+  ],
+}
